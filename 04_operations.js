4e538267a551c13c3c2cc7a8df8cbe8568995aee
Tanish Garg <tanishgarg94630@example.com>
2025-08-21 17:09:33 +0530

    change

--- 04_operations.js
+++ 04_operations.js
@@ -1,19 +1,49 @@
-let value =3
-let negValue = -value
-console.log(negValue);
+// let value =3
+// let negValue = -value
+// console.log(negValue);
 
-console.log(2+2);
-console.log(2-2);
-console.log(2*2);
-console.log(3**5);
-console.log(2/2);
-console.log(2%2);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(3**5);
+// console.log(2/2);
+// console.log(2%2);
 
 
-let str1 ="hello"
-let str2= " hitesh"
-let str3 = str1 + str2
-console.log(str3);
+// let str1 ="hello"
+// let str2= " hitesh"
+// let str3 = str1 + str2
+// console.log(str3);
 
-console.log("1"+2);
-console.log(1+"2");
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log((3+4)*5%3);
+
+// console.log(+true);
+// console.log(+"");
+// let num1, num2 ,num3
+// num1=num2=num3=2+2
+
+
+// let gameCounter =100
+// ++gameCounter;
+// console.log(gameCounter);
+
+// console.log(2>1);
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+
+console.log(null>0);
+console.log(null>=0);
+console.log(null==0);
+
+
+
+console.log(undefined>=0);
+console.log(undefined>0);
+console.log(undefined==0);
