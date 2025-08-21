006d8cacc0a242eb17176b482457f6be4b7f1a95
Tanish Garg <tanishgarg94630@example.com>
2025-08-21 15:07:00 +0530

    first file

---
+++ 02.datatypes.js
@@ -0,0 +1,14 @@
+"use strict";//treat all JS codeas newwer version
+
+//alert(3+3) // we are using node js ,not brwoser
+
+console.log(3+3);
+console.log("Hitesh")
+
+
+let name ="hitesh"
+let age =18
+let isLoggedIn = false
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
---
+++ 03_conversion.js
@@ -0,0 +1,22 @@
+// let score =undefined
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+//  let isLoggedIn = 12
+
+//  let booleanIsloogedIn =Boolean(isLoggedIn)
+
+//  console.log(booleanIsloogedIn)
+
+let someNumber =33
+let stringNumber = String(someNumber)
+
+console.log( typeof stringNumber);
