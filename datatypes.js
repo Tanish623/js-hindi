b024b5e8ff4676dd2c2263f5ab76e8fd366dc327
Tanish Garg <tanishgarg94630@example.com>
2025-08-21 17:29:18 +0530

    datatypes summary

---
+++ datatypes.js
@@ -0,0 +1,35 @@
+//#Primitive
+
+//7 types : string,Number,Boolean,null, undefined ,symbol
+
+
+// Reference (non primitime)
+
+//Array ,Objects ,functions
+
+
+const score =100
+const scoreValue = 100.3
+const isLoggedIIn =  false
+const outsideTemp = null
+let userEmail;
+const id =Symbol('123')
+const anotherId = Symbol('123')
+
+
+console.log(id===anotherId)
+
+const bigNumber =3333333333333325235252n
+
+const heros = ["shaktiman","naagraj","doga"]
+let myObj={
+ name :"hitesh",
+ age  : 22,
+
+}
+
+const myFunction = function (){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber)
